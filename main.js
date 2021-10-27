39a363c951683b21c1c9d1192ea33560b7f31dc6
Nicole Peiker <65312009+nicolepeiker@users.noreply.github.com>
2021-10-27 13:39:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const socialMediaLinks = {
+  github: 'nicolepeiker',
+  youtube: 'nicolerv1',
+  facebook: 'pablo.peiker',
+  instagram: 'nicolepeiker',
+  twitter: 'nicolerv'
+}
+
+function changeSocialMediaLinks () {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `http://www.${social}.com/${socialMediaLinks[social]}` 
+  }
+
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos () {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+ 
+  fetch(url)
+  .then (response => response.json())
+  .then (data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userGithub.href = data.html_url
+    userPhoto.src = data.avatar_url
+    userLogin.textContent = data.login
+  }) 
+}
+getGitHubProfileInfos ()
